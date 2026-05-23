9bb03e32ab8d04aaa48527c189cc3aa5bfca43b1
Laksito Pamilih <laksito@protonmail.com>
2026-05-23 22:28:36 +0700

    chore: nuke project

--- src/routes/master/product-categories/index.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/master/product-categories/')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/master/product-categories/"!</div>
-}
--- src/types/domain.ts
+++
@@ -1,6 +0,0 @@
-export interface SaleItemCandidate {
-  id: string;
-  name: string;
-  qty: number;
-  price: number;
-}
