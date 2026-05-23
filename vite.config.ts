59d0af3eb6913254ebc3217446d42d3c687e58a4
Laksito Pamilih <laksito@protonmail.com>
2026-05-23 21:21:41 +0700

    feat: add shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,4 +15,10 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
